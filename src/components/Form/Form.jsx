23b75204cb4aba43c85d88938907bc459c24bd82
babboeCodes <babboecodes@gmail.com>
2023-04-20 09:43:13 +0100

    refactor: refactor form component - move logic into index file

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,22 +1,27 @@
-import React, { useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { formSubmitHandler, disableBtnHandler } from './Form'
+import React from 'react';
+import formLogic, { formSubmitHandler, disableBtnHandler } from './index';
 import Button from '../Button/Button';
-import FormContext from '../../context/Form-Context/FormContext';
-import classes from './Form.module.css'
+import classes from './Form.module.css';
 
-const Form = ( { children, submit, success } ) => {
-   const navigate = useNavigate();
-   const context = useContext( FormContext )
-   const disabled = context.inputData[ children.props.title ]?.showMessage
-
-   const handleDisable = context.inputData[ children.props.title ]?.isInputValid ? null : 'disabled'
-   const isEmpty = context.inputData[ children.props.title ]?.isInputEmpty
+const Form = ({ children, submit, success }) => {
+   const { navigate, disabled, handleDisable, isEmpty } = formLogic(children);
 
    return (
-      <form className={ classes[ "form" ] } data-testid='form' onSubmit={ submit ? submit : ( e ) => formSubmitHandler( e, navigate, 'action' ) }>
-         { children }
-         <Button type="submit" disable={  disabled  && handleDisable } click={ ( e ) => isEmpty && disableBtnHandler( e ) } >Submit ✔</Button>
+      <form
+         className={classes['form']}
+         data-testid="form"
+         onSubmit={
+            submit ? submit : (e) => formSubmitHandler(e, navigate, 'action')
+         }
+      >
+         {children}
+         <Button
+            type="submit"
+            disable={disabled && handleDisable}
+            click={(e) => isEmpty && disableBtnHandler(e)}
+         >
+            Submit ✔
+         </Button>
       </form>
    );
 };
